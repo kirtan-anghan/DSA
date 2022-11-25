2568e790063162b46a93c0d053dca52c7f1fb7c1
kirtan <kjanghan2004@gmail.com>
2022-11-25 18:44:29 +0530

    first DSA program

---
+++ arrayprint.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+
+void printarr(int arr[],int size){
+    for ( int i = 0; i <= size-1; i++)
+    {
+        printf("%d \n",arr[i]);
+    }
+    
+}
+
+
+int main(){
+
+    int arr[]={1,2,3,4,5,6,7};
+    int size = sizeof(arr)/sizeof(int); 
+ 
+    printarr(arr,size);
+
+     return 0;
+}
